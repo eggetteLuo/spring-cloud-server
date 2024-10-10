bc27813dad62085ed693dc0f75bb9d3ec518f979
EggetteLuo <3428237080@qq.com>
2024-10-11 07:50:49 +0800

    添加权限模块配置文件，并增加权限模块控制器

---
+++ auth-server/src/main/kotlin/com/xiaoluo/authserver/controller/AuthController.kt
@@ -0,0 +1,11 @@
+package com.xiaoluo.authserver.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+// 处理 http 请求，作为应用的入口点
+// 处理用户注册和登录请求
+@RestController
+@RequestMapping("/auth")
+class AuthController {
+}
